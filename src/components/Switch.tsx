16df3cd49eea8cf6927347df0d226a567b86ea33
Yellni99 <hil0099@naver.com>
2025-02-18 22:51:23 +0900

    enhance: section focus 기능 추가

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -21,7 +21,7 @@ export default function Switch({ id, checked, onChange }: Props) {
         className={classNames(
           "flex items-center rounded-full h-15 w-27 bg-gray400 px-3",
           "before:inline-block before:h-10 before:w-10 before:rounded-full before:bg-white",
-          "peer-checked: bg-main peer-checked:before:translate-x-11",
+          "peer-checked:bg-main peer-checked:before:translate-x-11",
           "transition-colors duration-300 before:transition-transform before:duration-300"
         )}
       />
